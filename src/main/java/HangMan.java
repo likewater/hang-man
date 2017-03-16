6efb920a718b9efce2511c5bab8da411fd0d970e
Blake Womack <lakebayomackway@gmail.com>
2017-03-16 15:36:26 -0700

    add initial file set and structure.

---
+++ src/main/java/HangMan.java
@@ -0,0 +1,5 @@
+public class HangMan {
+  public String isHangManWord(String input) {
+    return "dead";
+  }
+}
---
+++ src/test/java/HangManTest.java
@@ -0,0 +1,12 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class HangManTest() {
+
+  @Test
+  public void is_first_test() {
+    HangMan hangMan = new HangMan();
+    assertEquals();
+  }
+
+}
